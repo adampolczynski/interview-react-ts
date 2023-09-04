84fdd72d9c10b254ff1f02a4b6ea6c173cbc8e1c
beat <adam_polczynski@yahoo.com>
2023-09-04 21:09:07 +0200

    vite-env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_JSONBIN_MASTER_KEY: string
+  readonly VITE_JSONBIN_ACCESS_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
